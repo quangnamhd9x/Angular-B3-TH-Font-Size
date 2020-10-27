d5da3cef8af1132538cc8a78bbd4a6037bbd31bd
QuangNam <quangnamhd9x@gmail.com>
2020-10-27 09:49:18 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FontSizeEditorComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
